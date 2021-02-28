7f3067c7198e74db4dcf6a1fcc2abc7b797a9379
otako <tatatatakuma55@gmail.com>
2021-02-28 17:37:31 +0900

    fixed text_detect_score.py

--- tests/test_detect_score.py
+++ tests/test_detect_score.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
-sys.path.append(str(Path(__file__).parent.parent / "src"))
+sys.path.append(str(Path(__file__).parent.parent / "src/predict"))
 import src.predict.detect_score as detect_score
 
 class TestDetectScore(unittest.TestCase):
@@ -11,6 +11,15 @@ class TestDetectScore(unittest.TestCase):
         self.ds=detect_score.DetectScore()
 
     def test_text2score(self):
+        text="A 40"
+        set_num = self.ds.get_set_text(text)
+        self.assertEqual(2,set_num)
+
+        text="4 1 15\n6 1 15"
+        set_num = self.ds.get_set_text(text)
+        self.assertEqual(6,set_num)
+
+
         text="1 15 \n0\n0"
         game_a,game_b,score_a,score_b=self.ds.text2score(text)
         self.assertEqual("1",game_a)
@@ -52,6 +61,35 @@ class TestDetectScore(unittest.TestCase):
         self.assertEqual("",game_b)
         self.assertEqual("",score_a)
         self.assertEqual("",score_b)
+
+        text="4 1 15\n6 2 30"
+        game_a,game_b,score_a,score_b=self.ds.text2score(text)
+        self.assertEqual("1",game_a)
+        self.assertEqual("15",score_a)
+        self.assertEqual("2",game_b)
+        self.assertEqual("30",score_b)
+
+        text="4 6 4 15\n6 2 2 30"
+        game_a,game_b,score_a,score_b=self.ds.text2score(text)
+        self.assertEqual("4",game_a)
+        self.assertEqual("15",score_a)
+        self.assertEqual("2",game_b)
+        self.assertEqual("30",score_b)
+
+        text="6 4 6 4 15\n4 6 2 2 30"
+        game_a,game_b,score_a,score_b=self.ds.text2score(text)
+        self.assertEqual("4",game_a)
+        self.assertEqual("15",score_a)
+        self.assertEqual("2",game_b)
+        self.assertEqual("30",score_b)
+
+        text="5 6 4 6 4 15\n7 4 6 2 2 30"
+        game_a,game_b,score_a,score_b=self.ds.text2score(text)
+        self.assertEqual("4",game_a)
+        self.assertEqual("15",score_a)
+        self.assertEqual("2",game_b)
+        self.assertEqual("30",score_b)
+        
 
 if __name__ == "__main__":
     unittest.main()
