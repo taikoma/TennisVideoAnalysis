c0603ffe59a79b1820b5e8810a50385ecd243516
otako <tatatatakuma55@gmail.com>
2021-03-14 12:11:40 +0900

    under mentenance track_data.py

--- src/track_data.py
+++ src/track_data.py
@@ -33,6 +33,132 @@ class TrackData():
         self.all_track_ball_x = df["X"].astype(np.int64).values.tolist()
         self.all_track_ball_y = df["Y"].astype(np.int64).values.tolist()
 
+    def detect_front_hit_frame(self,f,x,y,NET_LINE,THRESH):
+        y_front_index=np.array(y>NET_LINE)
+        y_diff=np.diff(y)
+        y_diff_index=np.array(abs(y_diff)<THRESH)
+
+        index =y_front_index[:-1] & y_diff_index
+        temp=index.astype(np.int)
+        i_renzoku=[]
+        print(temp)
+        for i in range(len(temp)-3):
+            if((temp[i]*temp[i+1]*temp[i+2]>0)and (temp[i+3]==0)):
+                i_renzoku.append(1)
+            else:
+                i_renzoku.append(0)
+        i_renzoku=np.array(i_renzoku)
+        f_shift=f[3:]
+        front_hit_array=f_shift[:-1][i_renzoku>0]
+        print(front_hit_array)
+
+        x_front_hit_array=[]
+        y_front_hit_array=[]
+        for i in range(len(front_hit_array)):
+            x_front_hit_array.append(x[f==front_hit_array[i]][0])
+            y_front_hit_array.append(y[f==front_hit_array[i]][0])
+        return x_front_hit_array,y_front_hit_array
+
+    def ball_data2df(self,output_filename):
+
+        f = np.array(self.frame_array)
+        x = np.array(self.all_track_ball_x)
+        y = np.array(self.all_track_ball_y)
+
+        #パラメータ
+        NET_LINE=340
+        THRESH=10
+
+        y_rear_index=np.array(y<=NET_LINE)
+
+        #手前側コートの打点タイミング
+        x_front_hit_array,y_front_hit_array = self.detect_front_hit_frame(f,x,y,NET_LINE,THRESH)
+
+        #手前側コートのボール着地タイミング
+        #84 161 244
+        #ひとつ前がプラス変化が大きい　その次に変化が小さい
+        y_diff=np.diff(y)
+        y_diff_up_index=np.array(y_diff>20)
+        y_diff_small_index=np.array(abs(y_diff)<THRESH)
+        index=y_diff_up_index[:-1] & y_diff_small_index[1:]
+        temp=index.astype(np.int)
+        index=temp>0
+        f_shift=f[:-1]
+
+        front_bounce_array=f_shift[1:][index]
+        print(front_bounce_array)
+
+        x_front_bounce_array=[]
+        y_front_bounce_array=[]
+        for i in range(len(front_bounce_array)):
+            x_front_bounce_array.append(x[f==front_bounce_array[i]][0])
+            y_front_bounce_array.append(y[f==front_bounce_array[i]][0])
+
+
+        #奥側コートのボール着地タイミング
+
+        index =y_rear_index[:-1] & y_diff_index
+        temp=index.astype(np.int)
+
+        i_renzoku=[]
+        for i in range(len(temp)-3):
+            if((temp[i]*temp[i+1]*temp[i+2]>0)and (temp[i+3]==0)):
+                i_renzoku.append(1)
+            else:
+                i_renzoku.append(0)
+        i_renzoku=np.array(i_renzoku)
+        f_shift=f[3:]
+
+        back_bounce_array=f_shift[:-1][i_renzoku>0]
+        print(back_bounce_array)
+
+        x_back_bounce_array=[]
+        y_back_bounce_array=[]
+        for i in range(len(back_bounce_array)):
+            x_back_bounce_array.append(x[f==back_bounce_array[i]][0])
+            y_back_bounce_array.append(y[f==back_bounce_array[i]][0])
+
+
+        #奥側側コートの打点着地タイミング
+        #ボール着地タイミングからフレームが30以内の最小値
+        range_start=back_bounce_array
+        range_end=back_bounce_array+30
+        index_min_array=[]
+        for i in range(len(range_start)):
+            index_range=np.where((f>=range_start[i]) & (f<=range_end[i]))
+            index_min=np.argmin(np.array(y[index_range]))
+            index_min_array.append(f[f>=range_start[i]][index_min])
+        back_hit_array=index_min_array
+        print(back_hit_array)
+
+        x_back_hit_array=[]
+        y_back_hit_array=[]
+        for i in range(len(back_hit_array)):
+            x_back_hit_array.append(x[f==back_hit_array[i]][0])
+            y_back_hit_array.append(y[f==back_hit_array[i]][0])
+            
+            
+
+        labels=["Frame","HitBounce"]
+
+        label_front_hit=["Front_Hit"]*len(front_hit_array)
+        df1=pd.DataFrame({"Frame":front_hit_array,"HitBounce":label_front_hit,"X_Ball":x_front_hit_array,"Y_Ball":y_front_hit_array})
+
+        label_front_bounce=["Front_Bounce"]*len(front_bounce_array)
+        df2=pd.DataFrame({"Frame":front_bounce_array,"HitBounce":label_front_bounce,"X_Ball":x_front_bounce_array,"Y_Ball":y_front_bounce_array})
+
+        label_back_hit=["Back_Hit"]*len(back_hit_array)
+        df3=pd.DataFrame({"Frame":back_hit_array,"HitBounce":label_back_hit,"X_Ball":x_back_bounce_array,"Y_Ball":y_back_bounce_array})
+
+        label_back_bounce=["Back_Bounce"]*len(back_bounce_array)
+        df4=pd.DataFrame({"Frame":back_bounce_array,"HitBounce":label_back_bounce,"X_Ball":x_back_hit_array,"Y_Ball":y_back_hit_array})
+
+        df=pd.concat([df1,df2,df3,df4])
+        df.sort_values('Frame', inplace=True)
+        df=df.reset_index()
+        df.to_csv(output_filename)
+
+
     def load_track_data(self,filename):
         # print(filename)
         with codecs.open(filename, "r", "utf-8",errors="ignore") as csv_file:
@@ -47,12 +173,9 @@ class TrackData():
         self.track_player_b_y = df["Y_B_onC"].astype(np.float).values.tolist()
         self.track_hit_bounce = df["HitBounce"].values.tolist()
 
-
-
-
-
-
-
-        
-            
+if __name__ == "__main__":
+    td=TrackData()
+    td.load_ball_data("../data/ball-pos.csv")
+    output_filename="../data/track_frame-test.csv"
+    td.ball_data2df(output_filename)
 
--- tests/test_track_data.py
+++ tests/test_track_data.py
@@ -1,5 +1,6 @@
 import unittest
 # import pandas as pd
+import numpy as np
 
 import src.track_data as track_data
 
@@ -17,3 +18,20 @@ class TestTrackData(unittest.TestCase):
         filename="./data/track-data.csv"
         self.track_data.load_track_data(filename)
         self.assertEqual(1.46,self.track_data.track_ball_x[0])
+
+    def test_detect_front_hit_frame(self):
+        NET_LINE=340
+        THRESH=10
+        f=np.array([0,10,11,12,13,14,15,20,21,30])
+        x=np.array([0,1,2,3,4,5,6,7,8,9])
+        y=np.array([300,400,500,595,596,594,590,500,400,300])
+        x_array,y_array=self.track_data.detect_front_hit_frame(f,x,y,NET_LINE,THRESH)
+        self.assertEqual([6],x_array)
+        self.assertEqual([590],y_array)
+
+        f=np.array([])
+        x=np.array([])
+        y=np.array([])
+        x_array,y_array=self.track_data.detect_front_hit_frame(f,x,y,NET_LINE,THRESH)
+        self.assertEqual([],x_array)
+        self.assertEqual([],y_array)
